6edfb2e9b18e6f4e278b68f59673d32bde79bc37
Kate Trahan <33528934+katetrahan@users.noreply.github.com>
2018-03-06 09:59:12 -0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'R0buqUGc9Hz3BypSDnrpdlalV',
-  consumer_secret: 'LNlhYkWqUEmpzwIdi1prhL5AxPB0sRnSOkE7ZCycI9BVUwtLAS',
-  access_token_key: '3189513013-TmhhwEbmTrgg89A8bIZfVrEFhkqaZ0lGjYqSp2N',
-  access_token_secret: 'pEH10GGt8yEidqL2BsXqFl98PQQrF7OYSgEblQoBQpqZF'
-}
